53f8e03641cb2c78a2b08d963a09b2dd39092f7a
Victor <victor@ebavs.net>
2014-04-12 18:55:29 +0200

    resolving some conflict commits

--- app/scripts/views/HomeView.js
+++ app/scripts/views/HomeView.js
@@ -14,6 +14,16 @@ QuitoFrontend.Views = QuitoFrontend.Views || {};
     },
     helloworld: function() {
       console.log("hello.")
+    },
+    initialize : function() {
+
+//      var mapOptions = {
+//        center: new google.maps.LatLng(41.39479, 2.1487679),
+//        zoom: 14
+//      };
+//      var map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
+
+      initializeMap()
     }
 
   });
--- app/scripts/views/MapView.js
+++ app/scripts/views/MapView.js
@@ -16,11 +16,14 @@ QuitoFrontend.Views = QuitoFrontend.Views || {};
       console.log("MapView.")
     },
     initialize : function() {
-      var mapOptions = {
-        center: new google.maps.LatLng(41.39479, 2.1487679),
-        zoom: 14
-      };
-      var map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
+      
+//      var mapOptions = {
+//        center: new google.maps.LatLng(41.39479, 2.1487679),
+//        zoom: 14
+//      };
+//      var map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
+
+      initializeMap()
     }
 
   });
