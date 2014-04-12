652a5f1ed8ba0b6a5535dfe100823e548fb4b7dd
Chris Kelley <chris@vetula.com>
2014-04-12 17:15:30 +0200

    adding map view

--- app/scripts/views/MapView.js
+++ app/scripts/views/MapView.js
@@ -14,6 +14,13 @@ QuitoFrontend.Views = QuitoFrontend.Views || {};
     },
     helloworld: function() {
       console.log("MapView.")
+    },
+    initialize : function() {
+      var mapOptions = {
+        center: new google.maps.LatLng(41.39479, 2.1487679),
+        zoom: 14
+      };
+      var map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
     }
 
   });
