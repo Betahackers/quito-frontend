b9fbf41f552d9ff9f42e57bd72f94791e30c3831
Chris Kelley <chris@vetula.com>
2014-04-13 14:01:12 +0200

    point to correct itemView

--- app/scripts/views/ProfileListView.js
+++ app/scripts/views/ProfileListView.js
@@ -8,7 +8,7 @@ QuitoFrontend.Views = QuitoFrontend.Views || {};
   QuitoFrontend.Views.ProfileListView = Backbone.Marionette.CompositeView.extend({
 
     template: JST['app/scripts/templates/ProfileListView.hbs'],
-    itemView : ItemView,
+    itemView : QuitoFrontend.ProfileItemView
     itemViewContainer : '#incidents'
 
   });
